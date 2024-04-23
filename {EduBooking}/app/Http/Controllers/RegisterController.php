63dd579de6c4911dcfdfeaec73c4578c78e75549
Lenny <lenny.olaxbargain@ynov.com>
2024-04-23 09:19:34 +0200

    début signup/login

---
+++ {EduBooking}/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Authentifier l'utilisateur après son inscription
+        Auth::login($user);
+
+        return redirect()->intended('/home');
+    }
+}
