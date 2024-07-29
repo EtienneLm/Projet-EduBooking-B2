d4487c2bb7403ec56d3b9316432e2eef48983064
elemee <elemee@moulinroty.fr>
2024-07-29 14:00:32 +0200

    new front

--- {EduBooking}/resources/views/main.blade.php
+++ {EduBooking}/resources/views/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +8,48 @@
     <link rel="stylesheet" href="{{ asset('main.css') }}">
     <title>Main Page</title>
 </head>
+
 <body>
+
+    <div class="wrapper four">
+        <div class="type">
+            <h3 class="typing">EduBooking</h3>
+        </div>
+    </div>
+    @include('header')
+
+    <div class="container">
+        <div class="card">
+            <a href="{{ url('student_page') }}">Continue as a student</a>
+        </div>
+
+        <div class="card">
+            <a href="{{ url('teacher_page') }}">Continue as a teacher</a>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
+
+<!--  //? old code, to keep:  
     @include('header')
 
     <div class="container">
         <div class="card">
             <h2>I'm a student</h2>
             <div class="card-content">
-                <!-- <p>Welcome !</p> -->
                 <p>Choose this option to take appointments with your teachers on different subjects</p>
             </div>
             <a href="{{ url('student_page') }}">Continue as a student</a>
         </div>
+
         <div class="card">
             <h2>I'm a teacher</h2>
             <div class="card-content">
-                <!-- <p>Welcome !</p> -->
                 <p>Choose this option to see all the upcoming appointments you have with your students</p>
             </div>
             <a href="{{ url('teacher_page') }}">Continue as a teacher</a>
         </div>
-    </div>
-</body>
-</html>
+    </div> -->
